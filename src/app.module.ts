7a93792d9d3918968deec5136f160c6bdfb69dc8
Jan <jan.blaszczyk@protonmail.com>
2022-07-19 20:42:07 +0200

    Basic modules generated - auth, users, admin, hr. Passport and passport-local added to dependencies to package.json file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app/config.module';
 import { MysqlProviderModule } from './providers/mysql/provider.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './models/users/users.module';
+import { AdminModule } from './models/admin/admin.module';
+import { HrModule } from './models/hr/hr.module';
 
 @Module({
-  imports: [AppConfigModule, MysqlProviderModule],
+  imports: [AppConfigModule, MysqlProviderModule, AuthModule, UsersModule, AdminModule, HrModule],
   controllers: [],
   providers: [],
 })
