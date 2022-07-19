d88d402a62c0fafc39320503231b419acc96d89a
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-19 13:07:06 +0200

    Created basic directories structure and added app config module.

---
+++ src/config/app/config.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfiguration } from './configuration';
+import { AppConfigService } from './config.service';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfiguration],
+      validationSchema: Joi.object({
+        APP_NAME: Joi.string(),
+        APP_ENV: Joi.string()
+          .valid('development', 'production', 'tests')
+          .default('development')
+          .required(),
+        APP_PORT: Joi.number().required(),
+        APP_URL: Joi.string(),
+      }),
+    }),
+  ],
+  providers: [ConfigService, AppConfigService],
+  exports: [ConfigService, AppConfigService],
+})
+export class AppConfigModule {}
