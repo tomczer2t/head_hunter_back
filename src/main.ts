87e9dc41db42a92280db68bd8a38e55900041d42
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-21 23:57:25 +0200

    Installed required packages for jwt strategies and used cookie parser.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(AppConfigService);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   await app.listen(appConfig.port);
 }
 bootstrap();
