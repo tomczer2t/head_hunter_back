261c56105d88ccb35257ddb97edf9fca0e3d23a3
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-22 13:45:35 +0200

    Changed enum name from UserStatus to UserAccountStatus.

--- types/user/index.ts
+++ types/user/index.ts
@@ -1,2 +1,2 @@
 export * from './user-role.enum';
-export * from './user-status.enum';
+export * from './user-account-status.enum';
--- types/user/user-account-status.enum.ts
+++ types/user/user-account-status.enum.ts
@@ -1,4 +1,4 @@
-export enum UserStatus {
+export enum UserAccountStatus {
   PENDING = 'pending',
   ACTIVE = 'active',
 }
