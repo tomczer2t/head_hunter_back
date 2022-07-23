e9484bef9ffed111b45a09feb30e957273276d57
Jan <jan.blaszczyk@protonmail.com>
2022-07-23 20:31:28 +0200

    Incorporated validation of input data (type sensitive) in StudentUrlRegistrationDto

--- src/models/user/user.controller.ts
+++ src/models/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserUrlRegistrationDto } from './dto/student-url-registration.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post('url-registration')
+  urlRegistration(@Body() userUrlRegistrationDto: UserUrlRegistrationDto) {
+    this.userService.urlRegistration(userUrlRegistrationDto);
+  }
+}
