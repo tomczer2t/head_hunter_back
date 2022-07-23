daafcdf24c3c00cdb659625e4dc63a8538ccb5ff
Jan <jan.blaszczyk@protonmail.com>
2022-07-23 19:33:36 +0200

    Global variables implemented for DTO validation

---
+++ src/config/global/const-variables.global.ts
@@ -0,0 +1,12 @@
+export const USER_INPUT_EMAIL_MAX_LENGTH = 254; // https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address#:~:text=%22There%20is%20a%20length%20limit,total%20length%20of%20320%20characters.
+
+export const USER_INPUT_PASSWORD_MIN_LENGTH = 8;
+export const USER_INPUT_PASSWORD_MAX_LENGTH = 64; // min and max password length - https://www.auditboard.com/blog/nist-password-guidelines/
+
+export const USER_INPUT_FIRSTNAME_MIN_LENGTH = 2; // https://babynames1000.com/three-letter/
+export const USER_INPUT_FIRSTNAME_MAX_LENGTH = 50; // 35 characters recommended in GB for first name. Forename p. 7 https://webarchive.nationalarchives.gov.uk/ukgwa/+/http://www.cabinetoffice.gov.uk/media/254290/GDS%20Catalogue%20Vol%202.pdf
+
+export const USER_INPUT_LASTNAME_MIN_LENGTH = 2; // http://www.americanlastnames.us/articles/shortest-last-names.html
+export const USER_INPUT_LASTNAME_MAX_LENGTH = 50; // 70 characters recommended in GB for full name. Individual full name p. 8 https://webarchive.nationalarchives.gov.uk/ukgwa/+/http://www.cabinetoffice.gov.uk/media/254290/GDS%20Catalogue%20Vol%202.pdf
+
+export const USER_INPUT_GITHUB_USERNAME_MAX_LENGTH = 39; // https://github.com/join
---
+++ src/config/global/index.ts
@@ -0,0 +1,1 @@
+export * from './const-variables.global';
