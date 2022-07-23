d154c2f54f49cd18f5b4a4db72ca5d2e3c16f957
Jan <jan.blaszczyk@protonmail.com>
2022-07-23 12:07:46 +0200

    HrUrlRegistrationDto implemented. Decorator for password comparison added. Basic methods in hr.service.ts and hr.controller.ts related with registration added.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(AppConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(appConfig.port);
 }
 bootstrap();
---
+++ src/models/hr/dto/hr-url-registration.dto.ts
@@ -0,0 +1,28 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { MatchDecorator } from '../../../common';
+
+export class HrUrlRegistrationDto {
+  @IsOptional()
+  @IsEmail()
+  @MaxLength(255)
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(64) // min and max password length supported by https://www.auditboard.com/blog/nist-password-guidelines/
+  password: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(64)
+  @MatchDecorator('password', { message: 'Provided passwords are different' })
+  passwordConfirm: string;
+}
