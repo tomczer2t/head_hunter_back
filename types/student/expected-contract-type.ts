bb2e0b58a23320c127595e2173048a423aa0e82e
Jan <jan.blaszczyk@protonmail.com>
2022-07-24 15:24:45 +0200

    Code cleanup. const-variables.global.ts updated. Functionality transferred to student model.

--- types/student/expected-contract-type.ts
+++ types/student/expected-contract-type.ts
@@ -1,6 +1,6 @@
 export enum ContractType {
-  EMPLOYMENT = 'uop',
-  B2B = 'b2b',
-  MANDATE_OR_WORK = 'uz/uod',
-  NO_PREFERENCES = 'wszystkie',
+  EMPLOYMENT = 'Umowa o prace',
+  B2B = 'B2B',
+  MANDATE_OR_WORK = 'Umowa zlecenie lub umowa o dzielo',
+  NO_PREFERENCES = 'Dowolna',
 }
