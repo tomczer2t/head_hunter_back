ac0b68ae4ff57cbac886c6af1d750ce2cbc0ee82
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-24 09:31:17 +0200

    EsLint fix problems.

--- src/models/hr/dto/hr-url-registration.dto.ts
+++ src/models/hr/dto/hr-url-registration.dto.ts
@@ -1,13 +1,7 @@
-import {
-  IsNotEmpty,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 import { MatchDecorator } from '../../../common';
 
 export class HrUrlRegistrationDto {
-
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
