012d5665365ba6aa3a4633488acb3443b2edd4a1
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-25 16:41:16 +0200

    Method to creating new student info entity from csv file implemented in StudentService.

--- src/models/student/student.service.ts
+++ src/models/student/student.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { StudentCsv } from '../../../types';
+import { StudentInfoEntity } from './entities';
 import { StudentFormProfileDto } from './dto/student-form-profile.dto';
 
 @Injectable()
+export class StudentService {
+  async addStudentInfoFromCsvFile({
+    email,
+    ...restStudentData
+  }: StudentCsv): Promise<StudentInfoEntity> {
+    const studentInfo = new StudentInfoEntity();
+    for (const [key, value] of Object.entries(restStudentData)) {
+      studentInfo[key] = value;
+    }
+    await studentInfo.save();
+    return studentInfo;
+  }
+}
 export class StudentService {
   urlRegistration(studentFormProfileDto: StudentFormProfileDto) {
     console.log('studentUrlRegistrationDto', studentFormProfileDto);
