24a54fe369ff4579d11d833d69021d0179cb8a2c
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-25 09:25:28 +0200

    File filter implemented in FileInterceptor.

--- src/models/admin/admin.controller.ts
+++ src/models/admin/admin.controller.ts
@@ -8,12 +8,13 @@ import { AdminService } from './admin.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ParseCsvPipe } from '../../common/pipes';
 import { StudentCsv } from '../../../types';
+import { fileCsvFilter } from '../../common/utils';
 
 @Controller('/admin')
 export class AdminController {
   constructor(private adminService: AdminService) {}
 
-  @UseInterceptors(FileInterceptor('students'))
+  @UseInterceptors(FileInterceptor('students', { fileFilter: fileCsvFilter }))
   @Post('/import-students')
   addStudents(@UploadedFile(ParseCsvPipe) students: StudentCsv[]) {
     return this.adminService.addStudents(students);
