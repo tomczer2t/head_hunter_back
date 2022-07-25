6393e2fc645d630848d6e3ac98a8baf296ed1f0e
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-25 16:30:21 +0200

    New verficationToken field in UserEntity, passwordHash field changed to be nullable and implemented method in basic form to adding student from csv.

--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { StudentCsv } from '../../../types';
+import { UserEntity } from './entities';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async addStudentFromCsvFile(studentCsv: StudentCsv) {
+    const student = new UserEntity();
+    student.email = studentCsv.email;
+    student.verificationToken = this.getVerificationToken();
+    await student.save();
+  }
+
+  getVerificationToken() {
+    return uuid();
+  }
+}
