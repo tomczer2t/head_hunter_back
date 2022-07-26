abf184c6764823de3cfb3292ce14daafba4b984f
Tomasz Czerwiński <czerwitom@gmail.com>
2022-07-26 12:56:28 +0200

    Possible error types for addStudents method added.

--- types/admin/add-students-response.ts
+++ types/admin/add-students-response.ts
@@ -9,3 +9,24 @@ export interface AddStudentsResponse {
   successCount: number;
   message: string;
 }
+
+enum ErrorType {
+  INVALID_EXTENSION_FILE,
+  VALIDATION_PARSED_FILE,
+}
+
+interface BasicError {
+  massage: string;
+  statusCode: number;
+}
+
+interface ExtensionError extends BasicError {
+  errorType: ErrorType.INVALID_EXTENSION_FILE;
+}
+
+interface ValidationError extends BasicError {
+  errorType: ErrorType.VALIDATION_PARSED_FILE;
+  row: number;
+}
+
+export type AddStudentsError = ExtensionError | ValidationError;
