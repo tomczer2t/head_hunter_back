94bdafe5d6775fcf1ba968b0249cd3d922598a44
Tomasz Czerwiński <czerwitom@gmail.com>
2022-08-01 15:00:33 +0200

    Code cleanup and set public routes by UsePublic decorator.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,17 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { LoginDto } from './dto';
 import { AuthService } from './auth.service';
-import { Request, Response } from 'express';
-import { JwtAccessGuard, JwtRefreshGuard } from '../common/guards';
+import { Response } from 'express';
+import { JwtRefreshGuard } from '../common/guards';
 import { LoginResponse } from '../../types';
-import { GetUser } from '../common';
+import { GetUser, UsePublic } from '../common';
 import { UserEntity } from '../models/user/entities';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @UsePublic()
   @Post('/login')
   login(
     @Res({ passthrough: true }) res: Response,
@@ -27,11 +20,12 @@ export class AuthController {
     return this.authService.login(res, loginDto);
   }
 
-  @UseGuards(JwtAccessGuard)
   @Get('/logout')
   logout(@GetUser() user: UserEntity) {
     return user;
   }
+
+  @UsePublic()
   @UseGuards(JwtRefreshGuard)
   @Get('/refresh')
   refreshTokens(@GetUser('id') id: string) {
--- src/models/student/student.controller.ts
+++ src/models/student/student.controller.ts
@@ -1,18 +1,16 @@
-import { Body, Controller, Inject, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Inject, Patch } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentFormProfileDto } from './dto/student-form-profile.dto';
 import { GetUser } from '../../common';
 import { UserEntity } from '../user/entities';
-import { JwtAccessGuard } from 'src/common/guards';
 
-@Controller('student')
+@Controller('/student')
 export class StudentController {
   constructor(
     @Inject(StudentService) private readonly studentService: StudentService,
   ) {}
 
-  @UseGuards(JwtAccessGuard)
-  @Patch()
+  @Patch('/')
   updateStudentInfo(
     @Body() studentFormProfileDto: StudentFormProfileDto,
     @GetUser() user: UserEntity,
