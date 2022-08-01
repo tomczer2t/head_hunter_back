82cfeb67424cb60d22d4242b390f8ce7e51c5f71
Tomasz Czerwiński <czerwitom@gmail.com>
2022-08-01 14:53:06 +0200

    Set globally JwtAccessGuard.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { JwtAccessGuard } from './common/guards';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(AppConfigService);
+  const reflector = new Reflector();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +17,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.enableCors({ credentials: true });
+  app.useGlobalGuards(new JwtAccessGuard(reflector));
   await app.listen(appConfig.port);
 }
 bootstrap();
