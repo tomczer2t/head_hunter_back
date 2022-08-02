b13e9199669f7d75b1c956fdf1e7f427ce2d8c18
Tomasz Czerwiński <czerwitom@gmail.com>
2022-08-02 11:07:55 +0200

    Route for getting available students.

--- src/models/student/student.controller.ts
+++ src/models/student/student.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Inject, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentFormProfileDto } from './dto/student-form-profile.dto';
+import { GetUser } from '../../common/decorators';
 import { UserEntity } from '../user/entities';
 import { UserRole } from '../../../types';
 import { GetUser, SetAccessRole } from '../../common/decorators';
@@ -18,5 +26,10 @@ export class StudentController {
     @GetUser() user: UserEntity,
   ) {
     return this.studentService.updateStudent(studentFormProfileDto, user);
+  }
+
+  @Get('/')
+  listAvailable() {
+    return this.studentService.listAvailable();
   }
 }
