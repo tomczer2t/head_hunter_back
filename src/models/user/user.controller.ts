e787f1cef86277c8ec9b8d30739bae956c73c16b
Tomasz Czerwiński <czerwitom@gmail.com>
2022-08-03 11:21:15 +0200

    Set public route for registration.

--- src/models/user/user.controller.ts
+++ src/models/user/user.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRegistrationDto } from './dto/user-registration.dto';
 import { RegisterResponse } from '../../../types';
+import { UsePublic } from '../../common/decorators';
 
 @Controller('/user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UsePublic()
   @Post('/')
   register(
     @Body() registrationDto: UserRegistrationDto,
