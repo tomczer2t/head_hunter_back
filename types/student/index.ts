e7aa206776bba87d37f3d3d85992b20532ed906a
Tomasz Czerwiński <czerwitom@gmail.com>
2022-08-05 09:05:36 +0200

    Updated StudentOnInterviewList interface.

--- types/student/index.ts
+++ types/student/index.ts
@@ -3,3 +3,4 @@ export * from './student-status';
 export * from './expected-work-type';
 export * from './list-available-response';
 export * from './filtered-available-student';
+export * from './student-on-interview-list';
--- types/student/student-on-interview-list.ts
+++ types/student/student-on-interview-list.ts
@@ -1,10 +1,7 @@
 import { PickedStudentInfoProperties } from './filtered-available-student';
 import { StudentStatus } from './student-status';
 
-export interface StudentOnInterviewList
-  extends Omit<PickedStudentInfoProperties, 'studentInfoId'> {
-  firstName: string;
-  lastName: string;
+export interface StudentOnInterviewList extends PickedStudentInfoProperties {
   bookedUntil: string;
   studentStatus: StudentStatus;
   userId: string;
