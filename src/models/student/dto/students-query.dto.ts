306e4baaccd01470b3e10f9457dee5da197f7cd4
Tomasz Czerwiński <czerwitom@gmail.com>
2022-08-09 09:33:15 +0200

    Basic students query dto created.

---
+++ src/models/student/dto/students-query.dto.ts
@@ -0,0 +1,32 @@
+import {
+  ListStudentsQueries,
+  SortBy,
+} from '../../../../types/student/list-students-queries';
+import { IsArray, IsEnum, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { ExpectedWorkType } from '../../../../types';
+
+export class StudentsQueryDto {
+  @IsOptional()
+  @IsEnum(SortBy)
+  sortBy?: SortBy;
+
+  @Transform(({ value: courseCompletionDegrees }) => {
+    const numberDegrees: number[] = [];
+    for (const stringDegree of courseCompletionDegrees) {
+      const numberDegree = Number(stringDegree);
+      if (isNaN(numberDegree)) break;
+      numberDegrees.push(numberDegree);
+    }
+    console.log(numberDegrees);
+    return numberDegrees;
+  })
+  @IsArray()
+  @IsOptional()
+  courseCompletion?: string[];
+
+  @Transform(({ value }) => decodeURI(value))
+  @IsOptional()
+  @IsEnum(ExpectedWorkType)
+  expectedTypeWork: ExpectedWorkType;
+}
